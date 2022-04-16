1b12ea7df66f7b211788728636dc2986ecf26675
kihiyuki <39235239+kihiyuki@users.noreply.github.com>
2022-04-17 01:03:40 +0900

    add config window

--- annotation/gui.py
+++ annotation/gui.py
@@ -1,8 +1,59 @@
 import subprocess
-from tkinter import Tk, ttk, messagebox
-
+from tkinter import (
+    Tk,
+    ttk,
+    messagebox,
+    Toplevel,
+    Entry,
+    END,
+    W,
+)
 from . import message
 from .data import Data
+
+
+class GridKw(object):
+    def __init__(self, maxcolumn=None, sticky=W) -> None:
+        self.row = 0
+        self.column = 0
+        self.columnspan = 1
+        self.maxcolumn = maxcolumn
+        self.sticky = sticky
+
+    def lf(self, n=1):
+        self.row += n
+        self.column = 0
+
+    def next(self):
+        self.column += 1
+        if self.maxcolumn is None:
+            pass
+        elif self.column >= self.maxcolumn:
+            self.lf()
+
+    def set(self, row=None, column=None, fullspan=None):
+        if row is not None:
+            self.row = row
+        if column is not None:
+            self.column = column
+        if (fullspan is not None) and (self.maxcolumn is not None):
+            if fullspan:
+                self.columnspan = self.maxcolumn
+            else:
+                self.columnspan = 1
+
+    def pull(self):
+        row = self.row
+        column = self.column
+        columnspan = self.columnspan
+        sticky = self.sticky
+        self.next()
+        return dict(
+            row = row,
+            column = column,
+            columnspan = columnspan,
+            sticky = sticky,
+        )
 
 
 def main(data: Data, config: dict) -> None:
@@ -25,55 +76,80 @@ def main(data: Data, config: dict) -> None:
             data.n = 0
             data.n_example = None
             data.deploy()
-
             data.n = config["n"]
             data.n_example = config["n_example"]
+
+    def _config(event=None):
+        cw = Toplevel()
+        cw.title("Config")
+        cw.resizable(False, False)
+        frm = ttk.Frame(cw, padding=20)
+        frm.grid()
+        gridkw = GridKw(maxcolumn=1)
+        entries = dict()
+        for k, v in config.items():
+            ttk.Label(frm, text=f"{k}: {message.CONFIG[k]}").grid(**gridkw.pull())
+            entries[k] = Entry(frm, width=50)
+            if v is None:
+                v = ""
+            elif type(v) is bool:
+                v = int(v)
+            elif type(v) is list:
+                v = ",".join([str(x) for x in v])
+            entries[k].insert(END, str(v))
+            entries[k].grid(**gridkw.pull())
+        ttk.Button(frm, text="Save", command=None).grid(**gridkw.pull())
+        ttk.Button(frm, text="Cancel", command=cw.destroy).grid(**gridkw.pull())
 
     data.load()
 
     root = Tk()
+    root.title("Annotation tool")
+    root.resizable(False, False)
     frm = ttk.Frame(root, padding=20)
     frm.grid()
 
-    row = 0
-    ttk.Label(frm, text="Annotation tool").grid(column=0, row=row, columnspan=2)
-    row += 1
-    ttk.Label(frm, text=f"------------------").grid(column=0, row=row, columnspan=2)
-
-    row += 1
+    gridkw = GridKw(maxcolumn=2)
+    gridkw.set(fullspan=True)
     for k in ["datafile", "workdir"]:
-        row += 1
-        ttk.Label(frm, text=f"{k}: {data.__getattribute__(k)}").grid(column=0, row=row, columnspan=2)
+        gridkw.lf()
+        ttk.Label(frm, text=f"{k}: {data.__getattribute__(k)}").grid(**gridkw.pull())
 
     for t in ["all", "annotated"]:
-        row += 1
-        ttk.Label(frm, text=f"{t}: {data.count(t)}").grid(column=0, row=row, columnspan=2)
+        gridkw.lf()
+        ttk.Label(frm, text=f"{t}: {data.count(t)}").grid(**gridkw.pull())
 
-    row += 1
-    row += 1
-    ttk.Label(frm, text=f"--- Annotation ---").grid(column=0, row=row, columnspan=2)
-    row += 1
-    ttk.Button(frm, text="[D]eploy", command=_deploy).grid(column=0, row=row)
-    ttk.Button(frm, text="[R]egister", command=_register).grid(column=1, row=row)
-    row += 1
-    ttk.Button(frm, text="[O]pen", command=_open).grid(column=0, row=row)
+    gridkw.lf(2)
+    ttk.Label(frm, text=f"--- Annotation ---").grid(**gridkw.pull())
 
-    row += 1
-    row += 1
-    ttk.Label(frm, text=f"----- Result -----").grid(column=0, row=row, columnspan=2)
-    row += 1
-    ttk.Button(frm, text="Deploy", command=_deploy_result).grid(column=0, row=row)
+    gridkw.set(fullspan=False)
+    gridkw.lf()
+    ttk.Button(frm, text="[D]eploy", command=_deploy).grid(**gridkw.pull())
+    ttk.Button(frm, text="[R]egister", command=_register).grid(**gridkw.pull())
+    gridkw.lf()
+    ttk.Button(frm, text="[O]pen", command=_open).grid(**gridkw.pull())
 
-    row += 1
-    row += 1
-    ttk.Label(frm, text=f"------------------").grid(column=0, row=row, columnspan=2)
-    row += 1
-    ttk.Button(frm, text="Config", command=None).grid(column=0, row=row)
-    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=row)
+    gridkw.set(fullspan=True)
+    gridkw.lf(2)
+    ttk.Label(frm, text=f"----- Result -----").grid(**gridkw.pull())
+
+    gridkw.set(fullspan=False)
+    gridkw.lf()
+    ttk.Button(frm, text="Deploy", command=_deploy_result).grid(**gridkw.pull())
+
+    gridkw.set(fullspan=True)
+    gridkw.lf(2)
+    ttk.Label(frm, text=f"------------------").grid(**gridkw.pull())
+
+    gridkw.set(fullspan=False)
+    gridkw.lf()
+    ttk.Button(frm, text="Config", command=_config).grid(**gridkw.pull())
+    ttk.Button(frm, text="[Q]uit", command=root.destroy).grid(**gridkw.pull())
 
     # keybind
     root.bind("d", _deploy)
     root.bind("r", _register)
     root.bind("o", _open)
+    root.bind("q", lambda e: root.destroy())
 
     root.mainloop()
--- annotation/message.py
+++ annotation/message.py
@@ -6,21 +6,22 @@ DEPLOY = "Deploy data to working directory"
 REGISTER = "Register annotation results to datafile"
 DEPROYRESULT = "Deploy results (all annotated images)"
 
-class CONFIG(object):
-    datafile = DATAFILE
-    workdir = WORKDIR
-    n = "Number of images to annotate at once (If null, deploy all images.)"
-    n_example = "Number of example images of each label(If null, deploy all images.)"
-    col_filename = "Column name for image-file name"
-    col_img = "Column name of image (containing numpy.ndarray)"
-    col_label = "Column name of label"
-    labels = "List of initial labels (comma-separated. If null, no initial labels set.)"
-    label_null = "String representing to be unannotated (If null, '' represens to be unannotated.)"
-    random = "Deploy randomly (1=Select randomly, 0=order by index)"
-    imgext = "Image file extension (including .)"
-    cmap = "matplotlib.cmap (you can use custom cmap name in cmap.py. If null, use default cmap of seaboarn.heatmap.)"
-    vmin = "seaborn.heatmap.vmin (If null, determined automatically.)"
-    vmax = "seaborn.heatmap.vmax (If null, determined automatically.)"
-    figsize = "matplotlib.pyplot.figure.figsize"
-    backup = "Backup (1=Copy datafile to datafile~ before save)"
-    verbose = "Verbose (1=Print verbose messages)"
+CONFIG = dict(
+    datafile = DATAFILE,
+    workdir = WORKDIR,
+    n = "Number of images to annotate at once (If null, deploy all images.)",
+    n_example = "Number of example images of each label(If null, deploy all images.)",
+    col_filename = "Column name for image-file name",
+    col_img = "Column name of image (containing numpy.ndarray)",
+    col_label = "Column name of label",
+    labels = "List of initial labels (comma-separated. If null, no initial labels set.)",
+    label_null = "String representing to be unannotated (If null, '' represens to be unannotated.)",
+    random = "Deploy randomly (1=Select randomly, 0=order by index)",
+    imgext = "Image file extension (including .)",
+    cmap = "matplotlib.cmap (you can use custom cmap name in cmap.py. If null, use default cmap of seaboarn.heatmap.)",
+    vmin = "seaborn.heatmap.vmin (If null, determined automatically.)",
+    vmax = "seaborn.heatmap.vmax (If null, determined automatically.)",
+    figsize = "matplotlib.pyplot.figure.figsize",
+    backup = "Backup (1=Copy datafile to datafile~ before save)",
+    verbose = "Verbose (1=Print verbose messages)",
+)
