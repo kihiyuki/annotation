b35c661c183e76e1a7108cf9fd06ba0490ebd0e6
kihiyuki <39235239+kihiyuki@users.noreply.github.com>
2022-04-16 22:15:36 +0900

    add lib.config test

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -28,19 +28,23 @@ def tempdir():
 def sampleconfig():
     config =  dict(
         DEFAULT = dict(
-            x = "xdef",
+            x = "dx",
         ),
         a = dict(
             x = "ax",
             y = "ay",
+            n = "1",
         ),
         b = dict(
             x = "bx",
             y = "by",
+            n = "2",
         ),
     )
     lib.config.save(config, file=CONFIGFILE, mode="overwrite")
+
     yield config
+
     Path(CONFIGFILE).unlink()
 
 
@@ -48,6 +52,49 @@ class TestConfig(object):
     def test_load(self, sampleconfig):
         config_load = lib.config.load(file=CONFIGFILE)
         assert config_load == sampleconfig
+
+        with pytest.raises(FileNotFoundError):
+            # notfound_ok = False
+            _ = lib.config.load(file=".INVALID")
+        config_load = lib.config.load(file=".INVALID", notfound_ok=True)
+        assert config_load == dict()
+
+    def test_load_section(self, sampleconfig):
+        config_load = lib.config.load(file=CONFIGFILE, section="a")
+        assert config_load == sampleconfig["a"]
+
+        with pytest.raises(KeyError):
+            _ = lib.config.load(file=CONFIGFILE, section="c")
+
+    def test_load_default(self, sampleconfig):
+        default = dict(n=11, m=12)
+        config_load = lib.config.load(file=CONFIGFILE, section="a", default=default)
+        config_load2 = lib.config.load(file=CONFIGFILE, default={"a": default})
+        c = sampleconfig.copy()
+        c["a"]["m"] = 12
+        assert config_load == c["a"]
+        assert config_load2 == c
+
+        # strict_key
+        with pytest.raises(KeyError):
+            _ = lib.config.load(file=CONFIGFILE, default={"a": default}, strict_key=True)
+
+        # strict_cast
+        with pytest.raises(ValueError):
+            _ = lib.config.load(file=CONFIGFILE, default={"a": dict(x=0)}, cast=True, strict_cast=True)
+
+        # cast
+        config_load = lib.config.load(file=CONFIGFILE, default={"a": default}, cast=True)
+        c = sampleconfig.copy()
+        c["a"]["n"] = int(c["a"]["n"])
+        c["a"]["m"] = 12
+        assert config_load == c
+
+    def test_save(self, sampleconfig):
+        # invalid mode
+        config = dict(hoge=dict(fuga=5))
+        with pytest.raises(ValueError):
+            lib.config.save(config, file=CONFIGFILE, mode="x")
 
     @pytest.mark.parametrize("mode", ["o", "OVERWRITE"])
     def test_save_overwrite(self, sampleconfig, mode):
@@ -86,7 +133,6 @@ class TestConfig(object):
                 c[section]["x"] = "addx"
             c[section]["z"] = "addz"
             assert c == config_load
-
 
     @pytest.mark.parametrize("mode", ["l", "leave", "c", "cancel", "n", "no"])
     def test_save_leave(self, sampleconfig, mode):
